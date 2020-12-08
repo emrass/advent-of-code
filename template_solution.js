9a58264575447c637b26904587a5de19d4459422
Enrico Mrass <emrass@users.noreply.github.com>
2020-12-07 20:59:34 -0800

    Get rid of process requirement in template

--- template_solution.js
+++ template_solution.js
@@ -1,22 +1,21 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function process(arr) {
-    return;
+  return;
 }
 
-fs.readFile(process.cwd() + '/input.txt', 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    const input = data.split('\n').map((entry) => {
-        return entry;
-    });
+fs.readFile(__dirname + "/input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const input = data.split("\n").map((entry) => {
+    return entry;
+  });
 
-    console.log(input);
+  console.log(input);
 
-    for (let i = 0; i < input.length; i++) {
-        // noop
-    }
+  for (let i = 0; i < input.length; i++) {
+    // noop
+  }
 });
-
